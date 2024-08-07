7f46bf5000d78dbe5963adc05b962f5b9a60a13e
hotsauceuniverse <jangsy6976@naver.com>
2024-08-07 18:09:52 +0900

    edit

--- app/src/main/java/com/example/test2/AppAdapter.java
+++ app/src/main/java/com/example/test2/AppAdapter.java
@@ -1,9 +1,13 @@
 package com.example.test2;
 
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,17 +18,25 @@ import java.util.ArrayList;
 public class AppAdapter extends RecyclerView.Adapter<AppAdapter.ViewHolder> {
 
     private ArrayList<AppItem> appItems;
+    private Context mContext;
+    private SparseBooleanArray selectedItems = new SparseBooleanArray();
+    private int prePosition = -1;
 
     @NonNull
     @Override
     public AppAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.app_activity, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AppAdapter.ViewHolder holder, int position) {
-        holder.onBind(appItems.get(position));
+        holder.onBind(appItems.get(position), position);
+
+        // 시간 범위 텍스트 설정
+        String timeRange = String.format("%02d시 ~ %02d시", position, (position + 1) % 24);
+        holder.timeTextView.setText(timeRange);
     }
 
     public void setAppItems(ArrayList<AppItem> list) {
@@ -34,27 +46,94 @@ public class AppAdapter extends RecyclerView.Adapter<AppAdapter.ViewHolder> {
 
     @Override
     public int getItemCount() {
-        return appItems == null ? 0 : appItems.size();
+        return 24; // 24시간에 맞게 아이템 개수를 설정
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView AppIcon;
         TextView AppUseTime;
         TextView AppName;
+        TextView timeTextView;
+        LinearLayout useTime;
+        private AppItem item;
+        private int position;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             AppIcon = itemView.findViewById(R.id.app_icon);
-//            AppUseTime = itemView.findViewById(R.id.app_use_time);
+            AppUseTime = itemView.findViewById(R.id.app_use_time);
             AppName = itemView.findViewById(R.id.app_name);
+            useTime = itemView.findViewById(R.id.use_time);
+            timeTextView = itemView.findViewById(R.id.time_textView);
         }
 
-        void onBind(AppItem item) {
+        void onBind(AppItem item, int position) {
+            this.item = item;
+            this.position = position;
+
             AppUseTime.setText(item.getAppTime());
             AppIcon.setImageDrawable(item.getAppIcon());
             AppName.setText(item.getAppName());
+
+            changeVisibility(selectedItems.get(position));
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+//            if (selectedItems.get(position)) {
+//                selectedItems.delete(position);
+//            } else {
+//                selectedItems.delete(prePosition);
+//                selectedItems.put(position, true);
+//            }
+//
+//            if (prePosition != -1) {
+//                notifyItemChanged(prePosition);
+//            }
+//
+//            notifyItemChanged(position);
+//            prePosition = position;
+
+            if (view.getId() == R.id.linearItem) {
+                if (selectedItems.get(position)) {
+                    selectedItems.delete(position);
+                } else {
+                    selectedItems.delete(prePosition);
+                    selectedItems.put(position, true);
+                }
+
+                if (prePosition != -1) {
+                    notifyItemChanged(prePosition);
+                    notifyItemChanged(position);
+                    prePosition = position;
+                }
+            }
+        }
+
+        private void changeVisibility(final boolean isExpanded) {
+            int dpValue = 150;
+            float d = mContext.getResources().getDisplayMetrics().density;
+            int height = (int) (dpValue * d);
+
+            // ValueAnimator.ofInt(int... values)는 View가 변할 값을 지정, 인자는 int 배열
+            ValueAnimator va = isExpanded ? ValueAnimator.ofInt(0, height) : ValueAnimator.ofInt(height, 0);
+
+            // Animation이 실행되는 시간, n/1000초
+            va.setDuration(600);
+            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(@NonNull ValueAnimator animation) {
+                    int value = (int) animation.getAnimatedValue();
+                    useTime.getLayoutParams().height = value;
+                    useTime.requestLayout();
+                    useTime.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
+                }
+            });
+            va.start();
         }
     }
 }
--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 
 import android.net.Uri;
@@ -28,40 +27,30 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.mikephil.charting.BuildConfig;
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
-import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckPackageNameThread checkPackageNameThread;
     boolean operation = false;
     private UsageStatsManager usageStatsManager;
+    private RecyclerView appRecyclerView;
+    private AppAdapter mAppAdapter;
     private Handler mHandler;
     private ImageView preBtn;
     private ImageView nextBtn;
     private TextView monthDay;
-    private BarChart barChart;
-    private AppAdapter mAppAdapter;
 
+    // 1초마다 자동 갱신
     private final Runnable mRunnable = new Runnable() {
         @Override
         public void run() {
@@ -75,12 +64,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // build.gradle defaultConfig minSdk 26 target upgrade
         CalendarUtil.selectDate = LocalDate.now();
 
         preBtn = findViewById(R.id.pre_btn);
         nextBtn = findViewById(R.id.next_btn);
         monthDay = findViewById(R.id.month_day);
-        barChart = findViewById(R.id.bar_chart);
+
+        setMonthDay();
 
         preBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,6 +91,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        appRecyclerView = findViewById(R.id.app_recyclerView);
+        appRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ArrayList<AppItem> userInstalledApps = getUserInstalledApps();
+        mAppAdapter = new AppAdapter();
+        mAppAdapter.setAppItems(userInstalledApps);
+        appRecyclerView.setAdapter(mAppAdapter);
+
         this.mHandler = new Handler();
 
         usageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
@@ -108,10 +106,12 @@ public class MainActivity extends AppCompatActivity {
         }
         mRunnable.run();
 
+        // 앱 시작 시 권한을 체크하고, 권한이 없으면 설정 화면으로 이동
         if (!checkPermission()) {
             Intent PermissionIntent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS, Uri.parse("package:" + getPackageName()));
             startActivity(PermissionIntent);
         } else {
+            // 권한이 있으면 데이터를 수집 시작
             operation = true;
             checkPackageNameThread = new CheckPackageNameThread();
             checkPackageNameThread.start();
@@ -127,13 +127,21 @@ public class MainActivity extends AppCompatActivity {
         monthDay.setText(monthDayFormat(CalendarUtil.selectDate));
     }
 
+    // 현재 포그라운드 앱 패키지 로그로 띄우는 함수
     private class CheckPackageNameThread extends Thread {
+
         public void run() {
+            // operation == true 일때만 실행
             while (operation) {
-                if (!checkPermission()) continue;
+                if (!checkPermission())
+                    continue;
+
+                // 현재 포그라운드 앱 패키지 이름 가져오기
                 Log.d("Current Package", getPackageName(getApplicationContext()));
                 try {
+                    // 2초마다 패키치 이름을 로그창에 출력
                     sleep(2000);
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -148,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
                 .getSystemService(Context.APP_OPS_SERVICE);
 
         int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
-                android.os.Process.myUid(), getApplicationContext().getPackageName());
+                Process.myUid(), getApplicationContext().getPackageName());
 
         if (mode == AppOpsManager.MODE_DEFAULT) {
             granted = (getApplicationContext().checkCallingOrSelfPermission(
@@ -160,120 +168,155 @@ public class MainActivity extends AppCompatActivity {
         return granted;
     }
 
+    // 자신의 앱의 최소 타겟을 롤리팝 이전으로 설정
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    // 현재 포그라운드 앱 패키지를 가져오는 함수
     public static String getPackageName(@NonNull Context context) {
+
+        // UsageStatsManager 선언
         UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+
+        // 마지막 실행 앱 타임스탬프
         long lastRunAppTimeStamp = 0L;
+
+        // 얼마만큼의 시간동안 수집한 앱의 이름을 가져오는지 정하기 (begin ~ end 까지의 앱 이름을 수집한다)
         final long INTERVAL = 1000 * 60 * 5;
         final long end = System.currentTimeMillis();
-        final long begin = end - INTERVAL;
+        final long begin = end - INTERVAL; // 5분전
 
-        LongSparseArray<String> packageNameMap = new LongSparseArray<>();
+        LongSparseArray packageNameMap = new LongSparseArray<>();
+
+        // 수집한 이벤트들을 담기 위한 UsageEvents
         final UsageEvents usageEvents = usageStatsManager.queryEvents(begin, end);
 
+        // 이벤트가 여러개 있을 경우 (최소 존재는 해야 hasNextEvent가 null이 아니니까)
         while (usageEvents.hasNextEvent()) {
+
+            // 현재 이벤트를 가져오기
             UsageEvents.Event event = new UsageEvents.Event();
             usageEvents.getNextEvent(event);
 
+            // 현재 이벤트가 포그라운드 상태라면(현재 화면에 보이는 앱이라면)
             if (isForeGroundEvent(event)) {
+
+                // 해당 앱 이름을 packageNameMap에 넣는다.
                 packageNameMap.put(event.getTimeStamp(), event.getPackageName());
 
+                // 가장 최근에 실행 된 이벤트에 대한 타임스탬프를 업데이트 해준다.
                 if (event.getTimeStamp() > lastRunAppTimeStamp) {
                     lastRunAppTimeStamp = event.getTimeStamp();
                 }
             }
         }
+        // 가장 마지막까지 있는 앱의 이름을 리턴해준다.
         return packageNameMap.get(lastRunAppTimeStamp, "").toString();
     }
 
+    // 앱이 포그라운드 상태인지 체크
     private static boolean isForeGroundEvent(UsageEvents.Event event) {
-        if (event == null) return false;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+
+        // 이벤트가 없으면 false 반환
+        if (event == null) {
+            return false;
+        }
+
+        // 이벤트가 포그라운드 상태라면 true 반환
+        if (BuildConfig.VERSION_CODE >= 29) {
             return event.getEventType() == UsageEvents.Event.ACTIVITY_RESUMED;
         }
+
         return event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND;
     }
 
+    // 앱 사용 시간을 계산하고 로그로 출력하는 함수
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//    private void showAppUsageStats(LocalDate date) {
+//        long endTime = getEndOfDay(date); // 현재 시간을 종료 시간으로 설정
+//        Log.d("endTime", String.valueOf(endTime));
+//
+//        long beginTime = getBeginOfDay(date); // 오늘 자정 시간을 시작 시간으로 설정
+//        Log.d("beginTime", String.valueOf(beginTime));
+//
+//        List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, beginTime, endTime);
+//
+//        if (stats != null) {
+//            ArrayList<AppItem> appItems = new ArrayList<>();
+////            for (UsageStats usageStats : stats) {     // for each 문 => for (각 요소 값 : 배열이나 컨테이너 값) { 반복 수행할 작업 }
+//            for (int i = 0; i < stats.size(); i++) {
+//                UsageStats usageStats = stats.get(i);
+//                long timeInForeground = usageStats.getTotalTimeInForeground();
+//                // 1초 = 1,000
+//                // 1분 = 1,000 * 60
+//                // 1시간 = 1,000 * 60 * 60
+//                // 1일 = 1,000 * 60 * 60 * 24
+//                int hours = (int) ((timeInForeground / (1000 * 60 * 60)) % 24);     // 24로 나눠 24시간 형식으로 시간 제한
+//                int minutes = (int) ((timeInForeground / (1000 * 60)) % 60);        // 60으로 나눠 분 단위를 0 ~ 59까지 제한
+//                int seconds = (int) (timeInForeground / 1000) % 60;                 // 60으로 나눠 초 단위를 0 ~ 59까지 제한
+//
+//                if (hours > 0 || minutes > 0 || seconds > 0) {
+//                    String packageName = usageStats.getPackageName();
+//                    PackageManager pm = getPackageManager();
+//                    String appName;
+//                    Drawable appIcon;
+//                    try {
+//                        appName = pm.getApplicationLabel(pm.getApplicationInfo(packageName, 0)).toString();
+//                        appIcon = pm.getApplicationIcon(packageName);
+//                    } catch (PackageManager.NameNotFoundException e) {
+//                        appName = packageName;
+//                        appIcon = getDrawable(R.mipmap.ic_launcher);
+//                    }
+//                    String usageTime = hours + "h:" + minutes + "m:" + seconds + "s";
+//                    appItems.add(new AppItem(appName, appIcon, usageTime, packageName));
+//                }
+//            }
+//            mAppAdapter.setAppItems(appItems);
+//        }
+//    }
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void showAppUsageStats(LocalDate date) {
-        long endTime = getEndOfDay(date);
-        long beginTime = getBeginOfDay(date);
+        long endTime = getEndOfDay(date); // 현재 시간을 종료 시간으로 설정
+        long beginTime = getBeginOfDay(date); // 오늘 자정 시간을 시작 시간으로 설정
 
         List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, beginTime, endTime);
 
+        // 24시간에 대한 시간 슬롯 리스트를 생성합니다.
+        ArrayList<AppItem> appItems = new ArrayList<>(24);
+        for (int i = 0; i < 24; i++) {
+            appItems.add(new AppItem("", null, "", "")); // 빈 값으로 초기화
+        }
+
         if (stats != null) {
-            ArrayList<BarEntry> entries = new ArrayList<>();
-            ArrayList<String> labels = new ArrayList<>();
-
-            ArrayList<AppItem> installedApps = getUserInstalledApps();
-            PackageManager pm = getPackageManager();
-            Map<String, String> appNameMap = new HashMap<>();
-
-            for (AppItem appItem : installedApps) {
-                appNameMap.put(appItem.getPackageName(), appItem.getAppName());
-            }
-
-            for (int i = 0; i < stats.size(); i++) {
-                UsageStats usageStats = stats.get(i);
+            for (UsageStats usageStats : stats) {
                 long timeInForeground = usageStats.getTotalTimeInForeground();
+                int hours = (int) ((timeInForeground / (1000 * 60 * 60)) % 24); // 24시간 형식으로 제한
 
-                int hours = (int) ((timeInForeground / (1000 * 60 * 60)) % 24);
-                int minutes = (int) ((timeInForeground / (1000 * 60)) % 60);
-                int seconds = (int) (timeInForeground / 1000) % 60;
-
-                if (hours > 0 || minutes > 0 || seconds > 0) {
+                if (timeInForeground > 0) {
                     String packageName = usageStats.getPackageName();
+                    PackageManager pm = getPackageManager();
                     String appName;
+                    Drawable appIcon;
                     try {
                         appName = pm.getApplicationLabel(pm.getApplicationInfo(packageName, 0)).toString();
+                        appIcon = pm.getApplicationIcon(packageName);
                     } catch (PackageManager.NameNotFoundException e) {
                         appName = packageName;
+                        appIcon = getDrawable(R.mipmap.ic_launcher);
                     }
-
-                    float usageTimeInSeconds = hours * 3600 + minutes * 60 + seconds;
-                    entries.add(new BarEntry(entries.size(), usageTimeInSeconds));
-                    labels.add(appName);
+                    String usageTime = hours + "h:" + ((timeInForeground / (1000 * 60)) % 60) + "m:" + (timeInForeground / 1000 % 60) + "s";
+                    appItems.set(hours, new AppItem(appName, appIcon, usageTime, packageName));
                 }
             }
-
-            BarDataSet dataSet = new BarDataSet(entries, "사용 시간(초)");
-            BarData barData = new BarData(dataSet);
-            barChart.setData(barData);
-
-            XAxis xAxis = barChart.getXAxis();
-            xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
-            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-            xAxis.setGranularity(1f);
-
-            // Y축 설정 (시간 단위로 정수 값 표시)
-            YAxis leftAxis = barChart.getAxisLeft();
-            leftAxis.setGranularity(1f); // Y축의 최소 단위, 정수 값으로 설정
-            leftAxis.setGranularityEnabled(true); // 최소 단위 사용을 강제
-            leftAxis.setAxisMinimum(0f); // Y축의 최소 값
-
-            leftAxis.setValueFormatter(new ValueFormatter() {
-                @Override
-                public String getFormattedValue(float value) {
-                    if (value == 0) {
-                        return "0시간";
-                    } else {
-                        // Y축의 값을 시간으로 변환
-                        int hours = (int) (value / 2999); // 3000을 기준으로 시간으로 변환
-                        return hours + "시간";
-                    }
-                }
-            });
-
-            barChart.getAxisRight().setEnabled(false);
-            barChart.setFitBars(true);
-            barChart.invalidate();
         }
+        mAppAdapter.setAppItems(appItems);
     }
 
+    // 특정 날짜의 자정 시간을 반환 (00시 00분 00초 000밀리초)
     private long getBeginOfDay(LocalDate date) {
         return date.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 
+    // 특정 날짜의 끝 시간을 반환 (23시 59분 59초 999밀리초)
     private long getEndOfDay(LocalDate date) {
         return date.plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli() - 1;
     }
